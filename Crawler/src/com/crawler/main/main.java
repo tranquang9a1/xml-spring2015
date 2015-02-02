82f42eefbdf48f45ac374350db8a2cfd94b7e392
quangtv.fuhcm <quangtv.fuhcm@5bbc97ca-2374-485f-320c-c957378d1ee8>
2015-02-02 13:59:38 +0000

    Update search function

--- Crawler/src/com/crawler/main/main.java
+++ Crawler/src/com/crawler/main/main.java
@@ -4,20 +4,6 @@ import com.crawler.controller.ParserFromIZManga;
 import com.crawler.controller.ParserFromKissManga;
 import com.crawler.controller.ParserFromMangaHead;
 import com.crawler.dao.StoryDAO;
-import com.mongodb.BasicDBList;
-import com.mongodb.BasicDBObject;
-import com.mongodb.DBCursor;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
-import java.util.logging.Logger;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 /*
  * To change this template, choose Tools | Templates
@@ -35,9 +21,9 @@ public class main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        ParserFromMangaHead.getStoriesMangaHead();
-//        ParserFromKissManga.getStoriesKissManga();
-//        ParserFromIZManga.getStoriesIZManga();
+          //ParserFromMangaHead.getStoriesMangaHead();
+        //ParserFromKissManga.getStoriesKissManga();
+        ParserFromIZManga.getStoriesIZManga();
 
     }
 
