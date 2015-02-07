ec26e0faaddff84177cd419a000312492c1bec62
quangtv.fuhcm <quangtv.fuhcm@5bbc97ca-2374-485f-320c-c957378d1ee8>
2015-02-07 14:55:20 +0000

    Add Access-Control-Allow-Origin

--- Service/src/main/java/com/manga/service/StoryService.java
+++ Service/src/main/java/com/manga/service/StoryService.java
@@ -3,6 +3,7 @@ package com.manga.service;
 import com.manga.dao.impl.StoryDaoImpl;
 import com.manga.domain.Story;
 import com.manga.dto.Stories;
+import com.manga.util.CreatePDF;
 import com.manga.util.DomainToDTO;
 import com.manga.util.RemoveUTF8;
 import com.sun.java.browser.plugin2.DOM;
@@ -14,6 +15,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.xml.ws.Response;
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.net.URLEncoder;
@@ -30,63 +35,91 @@ public class StoryService {
     @Autowired
     StoryDaoImpl storyDAO;
 
+
+    @RequestMapping(value="/", method = RequestMethod.GET)
+    public String index() {
+        return "hello";
+    }
+
+    @RequestMapping(value="/download", method = RequestMethod.GET, produces = "application/pdf")
+
+    public @ResponseBody void download(@RequestParam("story") String story, @RequestParam("chapter") String chapter,
+                                         HttpServletResponse response) {
+        try {
+            byte[] content = CreatePDF.create(story, chapter);
+            response.setContentLength(content.length);
+            response.getOutputStream().write(content);
+            response.getOutputStream().flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
     @RequestMapping(value = "/all",method = RequestMethod.GET, produces = MediaType.APPLICATION_XML_VALUE )
     public @ResponseBody
-    Stories getAllStory(@RequestParam("limit") int limit, @RequestParam("offset") int offset) {
+    Stories getAllStory(@RequestParam("limit") int limit, @RequestParam("offset") int offset, HttpServletResponse response) {
        List<Story> storyList = storyDAO.getAllStory(limit, offset);
+        response.addHeader("Access-Control-Allow-Origin", "*");
         return DomainToDTO.convertFromList(storyList);
     }
     @RequestMapping(produces = MediaType.APPLICATION_XML_VALUE, value="/update", method = RequestMethod.GET)
     public @ResponseBody Stories getNewUpdate(@RequestParam("limit") int limit,
-                                              @RequestParam("offset") int offset) {
+                                              @RequestParam("offset") int offset, HttpServletResponse response) {
         List<Story> storyList = storyDAO.getNewUpdate(limit, offset);
+        response.addHeader("Access-Control-Allow-Origin", "*");
         return DomainToDTO.convertFromList(storyList);
 
     }
 
     @RequestMapping(produces = APPLICATION_XML_VALUE, value="/getByName", method = RequestMethod.GET)
     public @ResponseBody Stories getByName(@RequestParam("name") String name, @RequestParam("limit") int limit,
-                                           @RequestParam("offset") int offset) {
+                                           @RequestParam("offset") int offset, HttpServletResponse response) {
 
         String search = RemoveUTF8.removeAccent(name.toLowerCase());
         List<Story> storyList = storyDAO.getByName(search, limit, offset);
+        response.addHeader("Access-Control-Allow-Origin", "*");
         return DomainToDTO.convertFromList(storyList);
     }
 
     @RequestMapping(produces = APPLICATION_XML_VALUE, value="/getByType", method = RequestMethod.GET)
     public @ResponseBody Stories getByType(@RequestParam("type") String type, @RequestParam("limit") int limit,
-                                           @RequestParam("offset") int offset) {
+                                           @RequestParam("offset") int offset, HttpServletResponse response) {
 
         List<Story> storyList = storyDAO.getByType(type, limit, offset);
-
+        response.addHeader("Access-Control-Allow-Origin", "*");
         return DomainToDTO.convertFromList(storyList);
     }
 
     @RequestMapping(produces = MediaType.APPLICATION_XML_VALUE, value="/getIzManga", method = RequestMethod.GET)
     public @ResponseBody Stories getIzManga(@RequestParam("limit") int limit,
-                                            @RequestParam("offset") int offset) {
+                                            @RequestParam("offset") int offset, HttpServletResponse response) {
         List<Story> storyList = storyDAO.getIzManga(limit, offset);
+        response.addHeader("Access-Control-Allow-Origin", "*");
         return DomainToDTO.convertFromList(storyList);
     }
 
     @RequestMapping(produces = MediaType.APPLICATION_XML_VALUE, value="/getKissManga", method = RequestMethod.GET)
     public @ResponseBody Stories getKissManga(@RequestParam("limit") int limit,
-                                              @RequestParam("offset") int offset) {
+                                              @RequestParam("offset") int offset, HttpServletResponse response) {
         List<Story> storyList = storyDAO.getKissManga(limit, offset);
+        response.addHeader("Access-Control-Allow-Origin", "*");
         return DomainToDTO.convertFromList(storyList);
     }
 
     @RequestMapping(produces = MediaType.APPLICATION_XML_VALUE, value="/getMangaHead", method = RequestMethod.GET)
     public @ResponseBody Stories getMangaHead(@RequestParam("limit") int limit,
-                                              @RequestParam("offset") int offset) {
+                                              @RequestParam("offset") int offset, HttpServletResponse response) {
         List<Story> storyList = storyDAO.getMangaHead(limit, offset);
+        response.addHeader("Access-Control-Allow-Origin", "*");
         return DomainToDTO.convertFromList(storyList);
     }
 
     @RequestMapping(value = "/getStory", method = RequestMethod.GET, produces = MediaType.APPLICATION_XML_VALUE)
-    public @ResponseBody Stories getStory(@RequestParam("name") String name) {
+    public @ResponseBody Stories getStory(@RequestParam("name") String name, HttpServletResponse response) {
         name = RemoveUTF8.removeAccent(name.toLowerCase());
         Story story = storyDAO.getStory(name);
+        response.addHeader("Access-Control-Allow-Origin", "*");
         return DomainToDTO.convertFromObject(story);
     }
 }
